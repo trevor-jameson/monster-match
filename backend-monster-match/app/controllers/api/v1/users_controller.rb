7b8fa29488fd7788a652167cdb7cfcf789e089c7
Trevor Jameson <github email address>
2018-10-26 10:07:38 -0400

    user controller bug fix. create user now renders the new user.

--- backend-monster-match/app/controllers/api/v1/users_controller.rb
+++ backend-monster-match/app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    @@user = User.create(user_params)
+    @user = User.create(user_params)
     render json: @user.to_json
   end
 
